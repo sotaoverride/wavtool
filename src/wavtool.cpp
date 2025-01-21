c42ee21a95b36af00c672372927cbf365237f0f4
Aatir Manzur <aatrapps@gmail.com>
2025-01-21 13:40:50 -0500

    rap fixes

--- src/wavtool.cpp
+++ src/wavtool.cpp
@@ -41,7 +41,7 @@ int main(int argc, char *argv[]) {
 	fread(&header, sizeof(header), 1, input);
 	header.numChannels=destChannel;
 	LOG_INFO("HEADER number of channels set to  %i \n", header.numChannels);
-//	LOG_IF_ERROR("header.numChannels != destChannel"," HEADER number of channels %i, Dest Channel  %i \n", header.numChannels, destChannel);
+	LOG_IF_ERROR("header.numChannels != destChannel"," HEADER number of channels %i, Dest Channel  %i \n", header.numChannels, destChannel);
 	
 	//Check if WAV file is valid
 	if  (strncmp(header.chunkID, "RIFF", 4) != 0 ||
@@ -49,7 +49,7 @@ int main(int argc, char *argv[]) {
 		strncmp(header.subchunk1ID, "fmt ", 4) !=0 ||
 		strncmp(header.subchunk2ID, "data", 4) !=0) {
 		printf("Invalid WAVE file.\n");
-		LOG_ERROR("Invalid WAVE file %s %s %s %s %s \n", header.chunkID, header.subchunk1ID, header.subchunk2ID, inputFileStr);
+		LOG_ERROR("Invalid WAVE file %s %s %s %s  \n", header.chunkID, header.subchunk1ID, header.subchunk2ID, inputFileStr);
 		return 1;
 	}
 
