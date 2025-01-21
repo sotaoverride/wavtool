8d5dccb3d9b371f6326c4b064a4bea8693c2b8f3
Aatir Manzur <aatrapps@gmail.com>
2025-01-21 12:44:36 -0500

    1st wav tool

---
+++ Makefile
@@ -0,0 +1,21 @@
+CFLAGS = -Wall -Wextra -Iinclude -std=c++17
+
+SRCDIR = src
+OBJDIR = build
+BINDIR = bin
+
+SOURCES = $(wildcard $(SRCDIR)/*.cpp)
+OBJECTS = $(patsubst $(SRCDIR)/%.cpp, $(OBJDIR)/%.o, $(SOURCES))
+
+TARGET = $(BINDIR)/program
+
+all: $(TARGET)
+
+$(TARGET): $(OBJECTS)
+        $(CC) $(CFLAGS) $(OBJECTS) -o $@
+
+$(OBJDIR)/%.o: $(SRCDIR)/%.cpp
+        $(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	rm -rf $(OBJDIR) $(TARGET)
---
+++ inc/logger.h
@@ -0,0 +1,13 @@
+#define LOG(level, msg, args...) \
+        fprintf(stderr, "%s %s:%d: " msg "\n", level, __FILE__,__LINE__,##args)
+#define LOG_DEBUG(msg, args...) LOG("DEBUG", msg,##args)
+#define LOG_ERROR(msg, args...) LOG("ERROR", msg,##args)
+#define LOG_WARNING(msg, args...) LOG("WARNING", msg,##args)
+#define LOG_INFO(msg, args...) LOG("INFO", msg, ##)
+
+#define LOG_IF_ERROR(condition, message, args...) \
+({                                                \
+        if (condition) {                          \
+                LOG_ERROR(message,##args);      \
+        }                                         \
+)}
